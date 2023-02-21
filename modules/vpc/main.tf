9815b53d35b39e77750ab6d2f5ae42fdc6167231
singh.shi <singh.shikha@northeastern.edu>
2023-02-21 18:12:54 -0500

    adding assignment4 changes

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -52,3 +52,83 @@ resource "aws_subnet" "private" {
 
 }
 
+resource "aws_instance" "ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.application.id]
+  subnet_id = aws_subnet.public[0].id
+  # other instance configuration parameters go here
+
+  # attach EBS volumes to the instance
+  ebs_block_device {
+    device_name = "/dev/sdf"
+    volume_size = 50
+    volume_type = "gp2"
+    delete_on_termination = true
+  }
+
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp2"
+    delete_on_termination = true
+  }
+  disable_api_termination = false
+  tags = {
+    Name = "ec2-instance"
+  }
+
+
+}
+
+
+resource "aws_security_group" "application" {
+  name_prefix = "application_sg_"
+  description = "Security group for hosting web applications"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ ingress {
+    from_port   = 3030
+    to_port     = 3030
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 3030
+    to_port     = 3030
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
