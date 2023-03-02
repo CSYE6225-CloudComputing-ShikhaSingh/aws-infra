8d4be340492b4c5efa11661a50e483517675ff8f
singh.shi <singh.shikha@northeastern.edu>
2023-03-02 12:40:42 -0500

    Final Commit

--- main.tf
+++ main.tf
@@ -21,8 +21,19 @@ module "vpc" {
   number_of_subnets = var.number_of_subnets
   region            = var.region
   vpc_name          = var.vpc_name
-  ami_id            = var.ami_id
+ // ami_id            = var.ami_id
   instance_type     = var.instance_type
+  db_username= var.db_username
+  db_password = var.db_password
+  db_name= var.db_name
+  db_instance= var.db_instance
+  db_engine= var.db_engine
+  db_engine_version= var.db_engine_version
+  db_port = var.db_port
+  allocated_storage= var.allocated_storage
+  availability_zone= var.availability_zone
+  identifier= var.identifier
+
 }
 
 
@@ -48,11 +59,51 @@ variable "profile" {
 
 }
 
-variable "ami_id" {
-  type = string
-}
+# variable "ami_id" {
+#   type = string
+# }
 
 variable "instance_type" {
   type = string
 }
 
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+
+}
+
+variable "db_name" {
+  type = string
+
+}
+variable "db_engine" {
+  type = string
+}
+variable "db_engine_version" {
+  type = number
+}
+variable "db_instance" {
+  type    = string
+  default = "db.t3.micro"
+}
+variable "db_port" {
+  type    = number
+  default = "5432"
+}
+
+variable "allocated_storage" {
+  type    = number
+  default = "10"
+}
+variable "identifier" {
+  type = string
+}
+variable "availability_zone" {
+  type    = string
+  default = "us-east-1a"
+}
